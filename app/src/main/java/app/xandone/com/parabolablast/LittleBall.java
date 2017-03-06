c03044920d9e76564b180d30653be62a3b7bebb0
xandone <765478955@qq.com>
2017-03-06 18:41:28 +0800

    init

---
+++ app/src/main/java/app/xandone/com/parabolablast/LittleBall.java
@@ -0,0 +1,12 @@
+package app.xandone.com.parabolablast;
+
+/**
+ * Created by xandone on 2017/3/6.
+ */
+public class LittleBall {
+    private String mColor;
+    private String mRadius;
+    private String mStarX, mStartY;
+    private String mEndX, mEndY;
+    private String mControlX, mControlY;
+}
