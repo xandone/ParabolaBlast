2ce66a376784c659e41af9ab906b1e8d15797f6c
xandone <765478955@qq.com>
2017-03-06 18:18:45 +0800

    Initial commit

---
+++ app/src/main/java/app/xandone/com/parabolablast/ParabolaView.java
@@ -0,0 +1,22 @@
+package app.xandone.com.parabolablast;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by xandone on 2017/3/6.
+ */
+public class ParabolaView extends View {
+    public ParabolaView(Context context) {
+        super(context, null);
+    }
+
+    public ParabolaView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public ParabolaView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
